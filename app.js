f2615b917851ee2e715dec16b7151f3539f75f9c
humEyra1064 <humeyraercanfurkanyilmaz@gmail.com>
2023-01-23 22:09:12 +0300

    html change

--- app.js
+++ app.js
@@ -1,43 +1,65 @@
-const taxRate = 0.18;
-const shippingPrice = 15;
+const taxtRate = 0.18;
+const shippingPrice = 20;
 const shippingFreePrice = 300;
 
-window.addEventListener("load",()=>{
-    localStorage.setItem("taxRate",taxRate);
-    localStorage.setItem("shippingPrice",shippingPrice);
-    localStorage.setItem("shippingFreePrice",shippingFreePrice);
+window.addEventListener("load", () => {
+    localStorage.setItem("taxtRate", taxtRate);
+    localStorage.setItem("shippingPrice", shippingPrice);
+    localStorage.setItem("shippingFreePrice", shippingFreePrice);
 
+    // sessionStorage.setItem("taxtRate", taxtRate);
+    // sessionStorage.setItem("shippingPrice", shippingPrice);
+    // sessionStorage.setItem("shippingFreePrice", shippingFreePrice);
 });
-//👆sayfa yüklendiğinde local storag'e taxRate ,shippingPrice ,shippingFreePrice verilerini gönderdik.Local storage ile session storage arasındaki fark şu :session storage de telefonu veya bilgisayarı kapatınca depolanan veriler silinmiş oluyor.localde veriler her durumda browser da saklı kalıyor.Local storage: JavaScript sitelerinin ve uygulamalarının son kullanma tarihi olmadan, bir web tarayıcısında anahtar/değer değerlerinin kaydedilmesine izin veren.
-// Session storage: Bir web tarayıcısında anahtar/değer değerleri yalnızca bir oturum süresi için tarayıcıya kaydedilmesine izin veren bir özelliktir. Yani sekme tarayıcısı kapatıldığında de yok eder
-const productsDiv = document.querySelector(".products"); //👆query selector ile classı products olan divi seçtik ve bir değişkene atadık
-productsDiv.addEventListener("click",(event)=>{
-    if(event.target.className == "fa-solid fa-minus"){
-        if(event.target.parentElement.querySelector(".quantity").innerText > 1){
-event.target.parentElement.querySelector(".quantity").innerText--
-        }else{
-            if(confirm(
-                `${
-                    event.target.parentElement.parentElement.querySelector("h2").innerText
-                } will be deleted`
-                
-            )){
-                event.target.closest(".product").remove();
+
+
+const productsDiv = document.querySelector(".products");
+
+productsDiv.addEventListener("click", (event) => {
+    if (event.target.className == "fa-solid fa-minus") {
+        if (event.target.parentElement.querySelector(".quantity").innerText > 1) {
+            event.target.parentElement.querySelector(".quantity").innerText--;
+        } else {
+            if (
+                confirm(
+                    `${
+                        event.target.parentElement.parentElement.querySelector("h2").innerText
+                } will be deleted!!!`
+                )
+            ) {
+                event.target.closest(".product").remove()
             }
         }
-    }else if(event.target.className == "fa-solid fa-plus"){
-        event.target.previousElementSibling.innerText++;
-        // event.target.querySelector(".quantity").innerText++;
-
-
-    }else if(event.target.className == "remove-product"){
-        event.target.closest(".product").remove();
-
+    } else if (event.target.className == "fa-solid fa-plus") {
+        event.target.parentElement.querySelector(".quantity").innerText++;
+    } else if (event.target.className == "remove-product") {
+        event.target.closest(".product").remove()
     }
-
-
-   
-
-
+    calculateProductPrice(event.target);
+    calculateCartPrice();
 });
-//target demek tıkladığım yeri yakala demek.capturing dıştan içe yakalamaya deniyor.eğer quantity dediğim şey 1 den büyükse parentını yakala ve innerText i birer birer azalt.
+
+const calculateProductPrice = (btn) => {
+    const productInfoDiv = btn.parentElement.parentElement;
+
+    const price = Number(productInfoDiv.querySelector(".product-price strong").innerText);
+    const quantity = Number(productInfoDiv.querySelector(".quantity").innerText);
+    const productTotalDiv = productInfoDiv.querySelector(".price")
+
+    productTotalDiv.innerText = (price * quantity).toFixed(2);
+}
+
+const calculateCartPrice = () => {
+    const productsTotalPricesDiv = document.querySelectorAll(".price");
+    const subtotal = [...productsTotalPricesDiv].reduce((acc, price) => acc + Number(price.innerText), 0);
+    const taxtPrice = subtotal * localStorage.getItem("taxtRate")
+    const shippingPrice = parseFloat(subtotal > 0 && subtotal < localStorage.getItem("shippingFreePrice") ?
+        localStorage.getItem("shippingPrice") :
+        0)
+    const totalCart = subtotal + taxtPrice + shippingPrice;
+
+    document.querySelector("#subtotalCart").innerText = subtotal.toFixed(2);
+    document.querySelector("#taxRateCart").innerText = taxtPrice.toFixed(2);
+    document.querySelector("#shippingCart").innerText = shippingPrice.toFixed(2);
+    document.querySelector("#totalCart").innerText = totalCart.toFixed(2);
+}
